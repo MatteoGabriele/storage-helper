69b71eb2c41bbc86f9ec7869ab0c537de1ad943c
Matteo Gabriele <matteo@blocklevel.nl>
2016-11-09 17:35:31 +0100

    chore(unit test) add auto watch switch for travis ci

--- test/karma.config.js
+++ test/karma.config.js
@@ -6,6 +6,7 @@ module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
     singleRun: false,
+    autoWatch: process.env.TRAVIS ? false : true,
     frameworks: ['mocha', 'chai'],
     reporters: ['mocha'],
     files: [
