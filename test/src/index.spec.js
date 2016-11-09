054404e15698c70a93c7df9899fdab62ec8eb02c
Matteo Gabriele <matteo@blocklevel.nl>
2016-11-09 16:17:33 +0100

    test(index) add test for storage-helper

---
+++ test/src/index.spec.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai'
+import { setItem, getItem, removeItem, clear } from '../../dist/storage-helper'
+
+describe('Storage helper methods', () => {
+  it ('should be able to save an item and retrieve the value', () => {
+    setItem('foo', 1)
+
+    expect(getItem('foo')).to.not.be.null
+  })
+
+  it ('should be able to parse a retrieved item value', () => {
+    setItem('foo', 1)
+
+    expect(getItem('foo', true)).to.equal(1)
+  })
+
+  it ('should be able to remove a single item', () => {
+    setItem('foo', 1)
+    setItem('bar', 2)
+    removeItem('foo')
+
+    expect(getItem('foo')).to.be.null
+    expect(getItem('bar')).to.not.be.null
+  })
+
+  it ('should be able to remove all items', () => {
+    setItem('foo', 1)
+    setItem('bar', 2)
+    clear()
+
+    expect(getItem('foo')).to.be.null
+    expect(getItem('bar')).to.be.null
+  })
+})
