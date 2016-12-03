e43a707035fdb5d0d454e8a0acfcfc20df2994e7
Matteo Gabriele <matteo@blocklevel.nl>
2016-12-03 12:28:44 +0100

    chore(comments): remove comments in build

--- config/build.js
+++ config/build.js
@@ -110,25 +110,3 @@ function logError (e) {
 function blue (str) {
   return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
 }
-
-// import babel from 'rollup-plugin-babel'
-// import uglify from 'rollup-plugin-uglify'
-//
-// let pkg = require('../package.json')
-// let external = Object.keys(pkg.dependencies)
-//
-// export default {
-//   entry: 'src/index.js',
-//   format: 'umd',
-//   exports: 'named',
-//   moduleName: 'storageHelper',
-//   dest: pkg['main'],
-//   external: external,
-//   plugins: [
-//     babel({
-//       babelrc: false,
-//       presets: ['es2015-rollup']
-//     })
-//     // uglify()
-//   ]
-// }
